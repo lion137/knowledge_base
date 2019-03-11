4e98388f39a0a2676f6408b1fc6fa1d008cf7d19
lion137 <lion137@gmail.com>
2019-03-11 16:48:50 +0000

    Create base.py

---
+++ base.py
@@ -0,0 +1,4 @@
+# knowledge database
+
+data = ["python packaging pakiety venv virtualnv https://4programmers.net/Mikroblogi/View/36041#entry-36041",
+		"fixed capacity stack c c++ https://github.com/lion137/knowledge_base/blob/master/fixed_capacity_stack.cpp"]
