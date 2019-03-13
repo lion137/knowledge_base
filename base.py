cf1788bffad6255c2091bacb7bd6f5fef2a9d604
Tomasz Mrzy <lion137@gmail.com>
2019-03-13 23:11:50 +0000

    triangle construct

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
 # knowledge database
 
 data = ["python packaging pakiety venv virtualnv https://4programmers.net/Mikroblogi/View/36041#entry-36041",
-		"fixed capacity stack c c++ https://github.com/lion137/knowledge_base/blob/master/fixed_capacity_stack.cpp"]
+		"fixed capacity stack c c++ https://github.com/lion137/knowledge_base/blob/master/fixed_capacity_stack.cpp", 
+		"triangle construtruct, trójkąt https://4programmers.net/Forum/C_i_C++/323414-c_trojkaty?p=1571934#id1571934"]
