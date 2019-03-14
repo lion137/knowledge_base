28bebeeea06ebcb834f9620e01311001ae069895
Tomasz Mrzy <lion137@gmail.com>
2019-03-14 16:03:38 +0000

    asymptotic

--- base.py
+++ base.py
@@ -2,4 +2,5 @@
 
 data = ["python packaging pakiety venv virtualnv https://4programmers.net/Mikroblogi/View/36041#entry-36041",
 		"fixed capacity stack c c++ https://github.com/lion137/knowledge_base/blob/master/fixed_capacity_stack.cpp", 
-		"triangle construtruct, trójkąt https://4programmers.net/Forum/C_i_C++/323414-c_trojkaty?p=1571934#id1571934"]
+		"triangle construtruct, trójkąt https://4programmers.net/Forum/C_i_C++/323414-c_trojkaty?p=1571934#id1571934", 
+		"analiza algorytmów algorithms asymptotic asymptotyczna http://wazniak.mimuw.edu.pl/index.php?title=Matematyka_dyskretna_1/Wyk%C5%82ad_9:_Asymptotyka"]
